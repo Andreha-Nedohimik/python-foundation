aa9d4f648ca10378cad431e42a479a6285df3d1f
Andreha-Nedohimik <89411269+Andreha-Nedohimik@users.noreply.github.com>
2022-07-20 20:44:00 +0500

    Add files via upload

--- 2.py
+++ 2.py
@@ -2,11 +2,4 @@ input_number = int(input('Введите время в секундах: '))
 sec = input_number % 60
 minuts = (input_number // 60) % 60
 hours = input_number // 3600
-n = '0'
-if sec < 10:
-    sec = n + str(sec)
-if minuts < 10:
-    minuts = n + str(minuts)
-if hours < 10:
-    hours = n + str(hours)
-print(f"Это время равно: {hours}:{minuts}:{sec}")
+print(f"Это время равно: {hours:02}:{minuts:02}:{sec:02}")
